0059d1178b4e1062c5053eb281ede3b333f3d3b8
Andrew <andrewcod749@gmail.com>
2014-09-18 16:24:31 -0400

    Documented code and added a method to check for an obstacle for later time optimization.

--- jmbot.py
+++ jmbot.py
@@ -1,21 +1,26 @@
 from myro import *
 
 init()
-
+# 0-7000 value for proximity of obstacle
 threshold = 4000
 
+setName("JMBOT");
 while 1:
-
+        #starts moving robot forward
 	forward(1, 0.2)
-
+        # if the obstacle is left or in the centre, then turn right
 	if (getObstacle("center")>threshold or getObstacle("left")>threshold):
 		while (getObstacle("center")>threshold or getObstacle("left")>threshold):
 			turnRight(1, 0.5)
 			forward(1, 0.5)
 			turnLeft(1, 0.5)
-
+        # if the obstacle is to the right, turn the robot left
 	elif (getObstacle("right")>threshold):
 		while (getObstacle("right")>threshold):
 			turnLeft(1, 0.5)
 			forward(1, 0.5)
 			turnRight(1, 0.5)
+# method that can be implemented to check if there is an object
+def checkIfObject():
+    if(getObstacle("right") >threshold or getObstacle("center")>threshold or getObstacle("left")>thresholdr )
+        return True;
