d0eb14adc9c122c08d7bf1abd3d6b5a8322cc152
Yu Chen Hou <houyuchen66@gmail.com>
2014-10-09 12:54:11 -0400

    added calibrate return function

--- calibration.py
+++ calibration.py
@@ -9,31 +9,34 @@ import tty
 import termios
 import threading, time
 
-def turn():
-    print "robot turning at speed .5 \n"
-    turnRight(.5, 1000)
-    print "robot stop turning\n"
-print "Please press ESC when the robot turns 360 degrees\n"
+def calibrate():
+
+  def turn():
+      print "robot turning at speed .5 \n"
+      turnRight(.5, 1000)
+      print "robot stop turning\n"
+  print "Please press ESC when the robot turns 360 degrees\n"
 
 
-def isData():
-    return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
+  def isData():
+      return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
-old_settings = termios.tcgetattr(sys.stdin)
-try:
-    tty.setcbreak(sys.stdin.fileno())
+  old_settings = termios.tcgetattr(sys.stdin)
+  try:
+      tty.setcbreak(sys.stdin.fileno())
 
-    start = time.time()
-    thread = threading.Thread(target=turn).start()
+      start = time.time()
+      thread = threading.Thread(target=turn).start()
 
-    while 1:
-            if isData():
-                    c = sys.stdin.read(1)
-                    if c == '\x1b':         # x1b is ESC
-                            stop()
-                            end = time.time()
-                            break
+      while 1:
+              if isData():
+                      c = sys.stdin.read(1)
+                      if c == '\x1b':         # x1b is ESC
+                              stop()
+                              end = time.time()
+                              break
 
-finally:
-    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
-    print str(end - start) + "| Angular speed: " + str((end - start)/360)
+  finally:
+      termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
+      print str(end - start) + "| Angular speed: " + str((end - start)/360)
+      return (end - start)/360
--- server/app.js
+++ server/app.js
@@ -2,14 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var commandQueue = [];
+
+function isValidCommand(msg){
+    var command = msg.toLowerCase().split(" "); //split the message
+    var operator = command[0]; //First word of the command
+    switch(msg):
+            case(operator=="move")
+}
+
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendfile('/templates/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+    console.log('a user connected');
+    socket.on('command', function(msg){
+        commandQueue.push(msg);
+        io.emit('command', msg);
+    });
 });
 
-http.listen(3000, function(){
-  consgitole.log('listening on *:3000');
+http.listen(process.env.PORT||3000, function(){
+  console.log('listening on port '+ process.env.PORT||3000);
 });
