6223a57762d0715db508970e7fbdaf15b63ab933
Dave Pagurek <dave@davepagurek.com>
2014-10-25 12:32:25 -0400

    Threading in client code

--- client.py
+++ client.py
@@ -1,33 +1,39 @@
 import logging
 import json
+import threading
 import sched, time
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.WARNING)
 from socketIO_client import SocketIO
+
 from myro import *
 
 queue = []
 ready = True
+idle = True
 s = sched.scheduler(time.time, time.sleep)
 
 init("/dev/rfcomm1")
 with SocketIO("scribblerplaystwitch.herokuapp.com") as socket:
 
-  def check_ready():
+  def start_executer():
     global ready
     global s
-    if (ready):
-      apply_command()
-    else:
-      s.enter(0.1, 1, check_ready, ())
-      s.run()
+    while (1):
+      if (ready):
+        apply_command()
+      else:
+        time.sleep(0.1)
 
   def apply_command():
     global queue
     global s
     global ready
-    ready = False
+    global idle
     if len(queue)>0:
+      ready = False
+      idle = False
       selected = queue.pop(0)
+      queue = []
       command = selected["message"]
       username = selected["username"]
       if(command=="forward"):
@@ -42,34 +48,31 @@ with SocketIO("scribblerplaystwitch.herokuapp.com") as socket:
         speak("Im hooked on a feeling")
       take_photo()
       socket.emit("selected", {'username': username, 'message': command})
+      #time.sleep(2)
       ready = True
-      s.enter(0.1, 1, check_ready, ())
-      s.run()
 
   def take_photo():
-    print("pretend we're taking a picture")
     picture = takePicture("color")
     savePicture(picture, "static/stream.jpg")
     image_file = open("static/stream.jpg", "rb")
     data = image_file.read()
     socket.emit("photo", data.encode("base64"))
     image_file.close()
+    
+  def valid_command(command):
+    if (command=="forward" or command=="backward" or command=="right" or command=="left" or command=="hasselhoff"):
+      return True
+    else:
+      return False
 
   def on_command(*args):
     global queue
-    global ready
-    #print 'Command sent: ', args
-    #do command
-    #print "\n\n\n\n"
-    #command=args[0]["message"]
-    #user=args[0]["username"]
-    #print command
-    #print "\n\n\n\n\n"
-    queue.append(args[0])
-    if (ready):
-      apply_command()
-
-
-
+    if (valid_command(args[0]["message"])):
+      queue.append(args[0])
+  
+  executer = threading.Thread(target=start_executer, args = ())
+  executer.daemon = True
+  executer.start()
+  
   socket.on("command", on_command)
   socket.wait()
