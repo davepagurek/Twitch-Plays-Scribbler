e9b6c4de9b20df198216efa2cb9b9d8537383e49
Andrew Codispoti <andrewcod749@gmail.com>
2014-10-26 13:05:46 -0400

    Added notes in playsampledictionary with first part of Hooked on a Feeling

--- playsampledictionary.py
+++ playsampledictionary.py
@@ -2,5 +2,5 @@ from myro import *
 init("/dev/rfcomm1")
 from audiojm import *
 j=audiojm()
-notearray=[Note(1,audiojm.A5),Note(1,audiojm.A4)]
+notearray=[Note(0.2,audiojm.B4),Note(0.4,audiojm.D5),Note(0.4,audiojm.D5),Note(0.4,audiojm.D5),Note(0.4,audiojm.E5),Note(3.5*0.4,audiojm.D5)]
 j.playNoteDictionary(notearray)
